ce635b33123af8af6e94a9566e2b18c49c771157
Ogawa <uncle.sum1961@gmail.com>
2018-08-15 19:14:43 +0900

    暫定リザルトモーダル追加

---
+++ Assets/Scripts/Result/ResultModalModel.cs
@@ -0,0 +1,11 @@
+﻿
+public class ResultModalModel {
+
+    public readonly int Score;
+    public readonly float LeftTime;
+
+    public ResultModalModel(int score, float leftTime){
+        Score = score;
+        LeftTime = leftTime;
+    }
+}
---
+++ Assets/Scripts/Result/ResultModalPresenter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultModalPresenter : MonoBehaviour
+{
+
+    [SerializeField] private Text score;
+    [SerializeField] private Text leftTime;
+
+    public void Show(ResultModalModel model){
+        score.text = model.Score.ToString();
+        TimeSpan ts = new TimeSpan(0,0,Mathf.RoundToInt(model.LeftTime));
+        leftTime.text = string.Format("{0:D2}:{1:D2}", ts.Minutes, ts.Seconds);
+        this.gameObject.SetActive(true);
+    }
+
+    public void OnRestart(){
+        this.gameObject.SetActive(false);
+    }
+
+}
