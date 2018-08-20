c7ea42c064ec0d0f1a6e4e9c596f81e02a2ac12c
Ogawa <uncle.sum1961@gmail.com>
2018-08-20 20:34:48 +0900

    パスからプレハブをインスタンス化するクラスの作成

---
+++ Assets/Scripts/ResourceLoader.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class ResourceLoader : MonoBehaviour {
+
+    private static ResourceLoader instance;
+    public static ResourceLoader Instance {
+        get {
+            if(instance == null)
+                instance = new ResourceLoader();
+            return instance;
+        }
+    }
+
+    /// <summary>
+    /// 外部からのインスタンスの作成の禁止
+    /// </summary>
+    private ResourceLoader() { }
+
+
+    /// <summary>
+    /// pathと親を指定してプレハブをインスタンス化するクラス
+    /// </summary>
+    /// <returns>The create.</returns>
+    /// <param name="path">Path.</param>
+    /// <param name="parent">Parent.</param>
+    /// <typeparam name="T">The 1st type parameter.</typeparam>
+    public T Create<T>(string path, Transform parent){
+        GameObject prefab = (GameObject)Resources.Load(path);
+
+        GameObject instanceObject = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+        instanceObject.transform.SetParent(parent, false);
+
+        T component = instanceObject.GetComponent<T>();
+
+        return component;
+    }
+
+}
